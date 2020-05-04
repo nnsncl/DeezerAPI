37b9b16d2554400c7974d08ced48e125606dc0d5
NunesNicolas <nicolas.mgta@gmail.com>
2020-05-05 01:12:32 +0200

    Hotfix : userInpit become userInput

---
+++ js/likes.js
@@ -0,0 +1,60 @@
+$(document).ready(() => {
+    $( "#search-form" ).submit((e) => {
+        e.preventDefault()
+
+        const userInput = $('#user-input').val()
+        const filters = $('#filter-input').find('option:selected')
+        let filteredOption = filters.val()
+
+        switch (filteredOption) {
+            case 'song':
+                filteredOption = 'TRACK_ASC'
+                break;
+            case 'artist':
+                filteredOption = 'ARTIST_ASC'
+                break;
+            case 'album':
+                filteredOption = 'ALBUM_ASC'
+                break;
+            case 'rank':
+                filteredOption = 'RANKING'
+                break;
+            default:
+                filteredOption = 'TRACK_ASC'
+        }
+
+        $.ajax({
+            url : "https://api.deezer.com/search?q=" + userInput + "&order=" + filteredOption +"&output=jsonp",
+            dataType : 'jsonp'
+        }).done((tracks) => {
+
+            if(userInput.length != 0) {
+
+                for (let i = 1; i < tracks.data.length; i++) {
+                    let trackCover = tracks.data[i].album.cover
+                        trackTitle = tracks.data[i].title
+                        trackArtist = tracks.data[i].artist.name
+                        trackAlbum =  tracks.data[i].album.title
+                        trackPlayer = tracks.data[i].preview
+                    
+                        // Cards builder
+                        $("#cards-stack").append("<div id=card" + [i] + " class=card></div>");
+                        $("#card"+[i]).append("<div id=card-header" + [i] + " class=card-header>"
+                        + "<img src=" + trackCover + " class=cover>"
+                        + "<a id=like><ion-icon name=heart></ion-icon></a></div>")
+
+                        $("#card"+[i]).append("<div id=desc-content" + [i] + "class=card-body>"
+                        + "<p><strong>" + trackTitle + " </strong></p>"
+                        + "<p>" + trackArtist + "</p>"
+                        + "<p>" + trackAlbum + "</p></div>")
+                        
+                        $("#card"+[i]).append("<audio controls id=audio-player" + [i] + " class=audio-player src=" + trackPlayer + "></audio></div>")
+
+                }
+            }
+            else {
+                $("#cards-stack").html("Aucun résultat")
+            }
+        })
+    })
+})
