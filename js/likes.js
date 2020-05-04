2e1d3c5b7a5df783838c47c16814bcaf1f120d1b
NunesNicolas <nicolas.mgta@gmail.com>
2020-05-05 01:00:31 +0200

    Render APIs data to dom

--- js/likes.js
+++ js/likes.js
@@ -1,32 +1,60 @@
-$(document).ready(function(){
-    $("#search-form").submit(function(e){
+$(document).ready(() => {
+    $( "#search-form" ).submit((e) => {
         e.preventDefault()
 
-        // Define user input and filters
-        let userInput = $('#title-input').val()
-        let filters = $('#filter-input').find('option:selected')
-        let selectedFilter = filters.val()
+        const userInpit = $('#user-input').val()
+        const filters = $('#filter-input').find('option:selected')
+        let filteredOption = filters.val()
 
-        // Select filters and link it to Deezer API
-        switch (selectedFilter) {
-            case 'song' : selectedFilter = 'TRACK_ASC'
+        switch (filteredOption) {
+            case 'song':
+                filteredOption = 'TRACK_ASC'
                 break;
-            case 'artist' : selectedFilter = 'ARTIST_ASC'
+            case 'artist':
+                filteredOption = 'ARTIST_ASC'
                 break;
-            case 'album' : selectedFilter = 'ALBUM_ASC'
+            case 'album':
+                filteredOption = 'ALBUM_ASC'
                 break;
-            case 'rank' : selectedFilter = 'RANKING'
+            case 'rank':
+                filteredOption = 'RANKING'
                 break;
+            default:
+                filteredOption = 'TRACK_ASC'
         }
 
         $.ajax({
-            url : "https://api.deezer.com/search?q=" + userInput + "&order=" + selectedFilter +"&output=jsonp",
+            url : "https://api.deezer.com/search?q=" + userInpit + "&order=" + filteredOption +"&output=jsonp",
             dataType : 'jsonp'
-        }).done(function(tracks) { 
-            console.log(tracks)
+        }).done((tracks) => {
 
-           
+            if(userInpit.length != 0) {
+
+                for (let i = 1; i < tracks.data.length; i++) {
+                    let trackCover = tracks.data[i].album.cover
+                        trackTitle = tracks.data[i].title
+                        trackArtist = tracks.data[i].artist.name
+                        trackAlbum =  tracks.data[i].album.title
+                        trackPlayer = tracks.data[i].preview
+                    
+                        // Cards builder
+                        $("#cards-stack").append("<div id=card" + [i] + " class=card></div>");
+                        $("#card"+[i]).append("<div id=card-header" + [i] + " class=card-header>"
+                        + "<img src=" + trackCover + " class=cover>"
+                        + "<a id=like><ion-icon name=heart></ion-icon></a></div>")
+
+                        $("#card"+[i]).append("<div id=desc-content" + [i] + "class=card-body>"
+                        + "<p><strong>" + trackTitle + " </strong></p>"
+                        + "<p>" + trackArtist + "</p>"
+                        + "<p>" + trackAlbum + "</p></div>")
+                        
+                        $("#card"+[i]).append("<audio controls id=audio-player" + [i] + " class=audio-player src=" + trackPlayer + "></audio></div>")
+
+                }
+            }
+            else {
+                $("#cards-stack").html("Aucun résultat")
+            }
         })
     })
 })
-
