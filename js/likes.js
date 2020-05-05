d2c66cb331ee2625d1c33221c6b2f1624b96f141
NunesNicolas <nicolas.mgta@gmail.com>
2020-05-05 09:46:34 +0200

    update conditional state for search engine

--- js/likes.js
+++ js/likes.js
@@ -2,10 +2,11 @@ $(document).ready(() => {
     $( "#search-form" ).submit((e) => {
         e.preventDefault()
 
-        let userInpit = $('#user-input').val()
+        const userInput = $('#user-input').val()
         let filters = $('#filter-input').find('option:selected')
         let filteredOption = filters.val()
 
+        // Define how to filter datas
         switch (filteredOption) {
             case 'song':
                 filteredOption = 'TRACK_ASC'
@@ -16,6 +17,9 @@ $(document).ready(() => {
             case 'album':
                 filteredOption = 'ALBUM_ASC'
                 break;
+            case 'rating':
+                    filteredOption = 'RATING_ASC'
+                    break;
             case 'rank':
                 filteredOption = 'RANKING'
                 break;
@@ -23,15 +27,20 @@ $(document).ready(() => {
                 filteredOption = 'TRACK_ASC'
         }
 
+
+
         $.ajax({
-            url : "https://api.deezer.com/search?q=" + userInpit + "&order=" + filteredOption +"&output=jsonp",
+            url : "https://api.deezer.com/search?q=" + userInput + "&order=" + filteredOption +"&output=jsonp",
             dataType : 'jsonp'
         }).done((tracks) => {
 
-            if(userInpit.length != 0) {
+            // Check if userInput is filled and if it returns datas
+            if(userInput.length != 0 && tracks.data.length != 0) {
 
+                // Refresh on new Load
                 $("#cards-stack").empty()
 
+                // Prepare Cards for stack
                 for (let i = 1; i < tracks.data.length; i++) {
                     let trackCover = tracks.data[i].album.cover
                         trackTitle = tracks.data[i].title
@@ -52,10 +61,11 @@ $(document).ready(() => {
                         
                         $("#card"+[i]).append("<audio controls id=audio-player" + [i] + " class=audio-player src=" + trackPlayer + "></audio></div>")
                 }
-                
-            }
+               
+            } 
+            // Render this if the field is empty
             else {
-                $("#cards-stack").html("Aucun résultat")
+                $("#cards-stack").html("Aucun resultat")
             }
         
         })
