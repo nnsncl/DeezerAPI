2762803eb642f211d056474dc7022f45c1dd4ef7
NunesNicolas <nicolas.mgta@gmail.com>
2020-05-06 02:29:13 +0200

    Fix : remove stagger from animation

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,9 @@
 function contentAnimation() {
 
   let tl = gsap.timeline()
-  tl.from('.main-navigation', { duration: .3, translateY: -10, opacity: 0, stagger:.4})
-  tl.from('.is-animated-slow', { duration: .5, translateY: 100, opacity: 0, stagger:.4 })
-  tl.from('.is-animated', { duration: .2, translateY: 10, opacity: 0, stagger:.4 })
+  tl.from('.main-navigation', { duration: .3, translateY: -10, opacity: 0})
+  tl.from('.is-animated-slow', { duration: .5, translateY: 100, opacity: 0})
+  tl.from('.is-animated', { duration: .2, translateY: 10, opacity: 0})
 }
 
 contentAnimation()
