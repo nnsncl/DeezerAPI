33ac1c4d7dc23f5ba5ebac1f0746bd7329dc0b54
NunesNicolas <nicolas.mgta@gmail.com>
2020-05-07 15:34:55 +0200

    Comment code

--- js/playlist.js
+++ js/playlist.js
@@ -4,9 +4,10 @@ $(document).ready(() => {
 
     const generatePlaylistDOM = () => {
 
+        // Create a new index to render each likes from Local Storage
         for (let i = 0; i < liked.length; i++) {
 
-            // Cards builder (Need reviews : Add delete from playlist)
+            // Cards builder using template string
             $(".cards-stack").append(
                 `<div id="card${i}" class="card">
                     <div id="card-header${i}" class="card-header">
@@ -22,10 +23,15 @@ $(document).ready(() => {
                     </div>
                     <audio controls="" id="audio-player${i}" class="audio-player" src="${liked[i].trackPlayer}"></audio>
                 </div>`)
-
+            
+            // Target the click event on click event
             $(`#like${i}`).on("click", liked[i].trackID, function (e) {
+
+                // Check if the ID of the clicked element match with a track ID from tracks in local Storage and remove it
                 liked = liked.filter(likedID => likedID.trackID !== e.data)
                 localStorage.setItem("like", JSON.stringify(liked))
+
+                // Remove the card relative the the ID removes in Local Storage
                 $(this).closest(".card").remove()
             })
 
